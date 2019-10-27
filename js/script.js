0c58e4e35e8d5e3b29462c3a6020bd78488f8d9d
mathieuquiniou <mathieu.quiniou@tuteuralheure.fr>
2019-10-27 12:40:37 +0100

    étape 3 du tp validée

--- js/script.js
+++ js/script.js
@@ -2,30 +2,37 @@
 // Fonction appelée lors du click du bouton
 function start() {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
-  // Appel de la fonction fetchTodayForecast
+  datas = document.getElementById("city-input").value;
+
+  if (datas) {
+    apiWeather = new API_WEATHER(datas);
+  }
+  else {
+    apiWeather = new API_WEATHER();
+  }
 
   apiWeather
-    .fetchTodayForecast()
-    .then(function(response) {
-      // Récupère la donnée d'une API
-      const data = response.data;
+  .fetchTodayForecast()
+  .then(function(response) {
+    // Récupère la donnée d'une API
+    const data = response.data;
 
-      // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
-      const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+    // On récupère l'information principal
+    const main = data.weather[0].main;
+    const description = data.weather[0].description;
+    const temp = data.main.temp;
+    const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
 
-      // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
-      document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
-    })
-    .catch(function(error) {
-      // Affiche une erreur
-      console.error(error);
-    });
+    // Modifier le DOM
+    document.getElementById('today-forecast-main').innerHTML = main;
+    document.getElementById('today-forecast-more-info').innerHTML = description;
+    document.getElementById('icon-weather-container').innerHTML = icon;
+    document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+
+
+  })
+  .catch(function(error) {
+    // Affiche une erreur
+    console.error(error);
+  });
 }
